d651d88efecb0a1a2f6e41c17969e3326caa91a4
Bogdan Kyuchukov <bogdankyuchuk@gmail.com>
2026-06-16 17:17:47 +0200

    Prevent subtree path on /

--- main.go
+++ main.go
@@ -5,13 +5,24 @@ import (
 	"net/http"
 )
 
-func home(w http.ResponseWriter, req *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox!"))
+}
+
+func viewSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Viewing a single snippet."))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Creating a snippet."))
 }
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/view", viewSnippet)
+	mux.HandleFunc("/create", createSnippet)
 
 	log.Print("Starting server on :4000")
 
