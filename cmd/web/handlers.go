3478d9f17dd507ce60f2b8b96aa612bf5789236b
Bogdan Kyuchukov <bogdankyuchuk@gmail.com>
2026-06-18 13:17:31 +0200

    Added html templates and static http.FileServer

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,17 +2,34 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
-	w.Header().Set("Content-Type", "application/json")
-	_, err := fmt.Fprint(w, `{"message": "Hello from Snippetbox!"}`)
+
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/home.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
 
 	if err != nil {
-		http.NotFound(w, r)
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.ExecuteTemplate(w, "base", nil)
+
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,10 +8,13 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippets/{id}", getSnippet)
 	mux.HandleFunc("GET /snippets/create", getSnippetForm)
 	mux.HandleFunc("POST /snippets", createSnippet)
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
 	log.Print("Starting server on :4000")
 
