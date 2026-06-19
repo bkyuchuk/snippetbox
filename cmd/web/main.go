60c57942dd5f583750b2fa80d13020a8ad4c3aa8
Bogdan Kyuchukov <bogdankyuchuk@gmail.com>
2026-06-19 16:46:12 +0200

    Using config and cli flags

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
+type config struct {
+	addr      string
+	staticDir string
+}
+
+var cfg config
+
 func main() {
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("./ui/static"))
+	fileServer := http.FileServer(http.Dir(cfg.staticDir))
 
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippets/{id}", getSnippet)
@@ -16,9 +24,14 @@ func main() {
 	mux.HandleFunc("POST /snippets", createSnippet)
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	log.Print("Starting server on :4000")
+	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
 
-	err := http.ListenAndServe(":4000", mux)
+	flag.Parse()
+
+	log.Printf("Starting server on %s", cfg.addr)
+
+	err := http.ListenAndServe(cfg.addr, mux)
 
 	log.Fatal(err)
 }
